108d06d0197a24515e29e8ed1793c458c8046e84
Abib <habibsh180@gmail.com>
2023-11-29 20:30:06 +0700

    membuat method untuk console

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $user = new User();
+
+            while (true) {
+                $rfid = $this->ask('Masukkan RFID');
+                $username = $this->ask('Masukkan username');
+                $name= $this->ask('Masukkan nama lengkap');
+
+                if($rfid == null && $username == null && $name == null){
+                    $this->info('data tidak boleh kosong');
+                }
+                
+                $getUser = $user->updateUser($rfid, $username, $name);
+            
+                if($getUser == null){
+                    $this->info('data gagal ditambahkan');
+                }else{
+                    $this->info('data berhasil ditambahkan');
+                }
+            }
+                
+    }
+}
