cb9c173f357bef93fde13d010c5220d64d6f185a
Abib <habibsh180@gmail.com>
2023-11-29 22:19:03 +0700

    menambahkan method cekAbsensi

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Absensi;
 
 class UserController extends Controller
 {
-    public function createUser()
+    public function cekAbsensi($rfid)
     {
-        
+        $absen = new Absensi();
+
+        $date = date("d-m-Y h:i:sa");
+        $absen->cekAbsensi($rfid, $date);
+
+        return response()->json([
+            'status' => 'Name tag telah terdeteksi',
+            'message' => 200,                
+           ]);
     }
 }
