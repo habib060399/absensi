8c8bcc63d9cb6e01c4b488002d6c28c1d3d09370
habib060399 <habibsh180@gmail.com>
2023-12-01 22:26:24 +0700

    memperbaiki code || menambahkan return untuk status

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,17 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Absensi;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function cekAbsensi($rfid)
     {
         $absen = new Absensi();
-        // $absen->getAbsen($rfid);
+        $user = new User();
+        $time_now = date("h:i:s");
+        $date_now = date("d-m-Y"); 
+        $get_date="";
+        $get_absen_user = $absen->getAbsen($rfid);
+        $get_user = $user->getUser($rfid);
+            
+            if($get_absen_user != null){
+                foreach($get_absen_user as $a){
+                    $get_date = $a->tanggal;
+                }
+                if($date_now == $get_date){
+                    return 2;
+                }else{
+                    $absen->setAbsen($rfid, $date_now, $time_now, $get_user->name);
+                    return 1;
+                }
+                // }
+            }else{
+                $absen->setAbsen($rfid, $date_now, $time_now, $get_user->name);
+                return 1;                
+            }
+            // return $status;
 
-        $date = date("d-m-Y h:i:sa");
-        $absen->setAbsen($rfid, $date);
+            // return 1;
 
-        
     }
 }
