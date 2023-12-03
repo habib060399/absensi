acc233149ae85b702bcebd21d6b0b207a5472e60
Abib <habibsh180@gmail.com>
2023-12-03 09:54:44 +0700

    feat: menambahkan route untuk get absen

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('absensi', [RfidController::class, 'store']);
+Route::get('absensi', [RfidController::class, 'index']);
