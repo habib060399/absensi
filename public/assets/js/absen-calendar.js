e8dd000f1cbc25b5e5ab881740ecb3f45caf0877
habib060399 <habibsh180@gmail.com>
2024-06-26 22:28:01 +0700

    menambahkan fitur pembatas penampil data

--- public/assets/js/absen-calendar.js
+++ public/assets/js/absen-calendar.js
@@ -5,6 +5,7 @@ var calendar = new FullCalendar.Calendar(calendarEl, {
         center: "title",
         right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
     },
+    editable: true,
 });
 
 calendar.render();
@@ -17,7 +18,26 @@ function calendarAbsen(data) {
             center: "title",
             right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
         },
+        dayMaxEventRows: true,
+        views: {
+            timeGrid: {
+                dayMaxEventRows: 6,
+            },
+        },
         events: data,
+        editable: true,
+        dateClick: function (info) {
+            $("#createEventModal").modal("show");
+            console.log(info);
+        },
+        eventClick: function (info) {
+            var eventObj = info.event;
+            console.log(info);
+            $("#modalTitle1").html(eventObj.title);
+            $("#modalBody1").html(eventObj._def.extendedProps.description);
+            $("#eventUrl").attr("href", eventObj.url);
+            $("#fullCalModal").modal("show");
+        },
     });
 
     calendar.render();
