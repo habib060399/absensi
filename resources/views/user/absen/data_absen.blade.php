59e3e5a66835e041531639c48946f9728a4231ae
habib060399 <habibsh180@gmail.com>
2024-06-29 22:13:36 +0700

    memperbaiki tampilan

--- resources/views/user/absen/data_absen.blade.php
+++ resources/views/user/absen/data_absen.blade.php
@@ -46,7 +46,10 @@
                     <h4 id="modalTitle1" class="modal-title"></h4>
                     <button type="button" class="btn-close" data-bs-dismiss="modal"><span class="visually-hidden">close</span></button>
                 </div>
-                <div id="modalBody1" class="modal-body"></div>
+                <div id="modalBody1" class="modal-body">
+                    <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Edit</button>
+                    <a type="button" class="btn btn-danger hapus_absen" id="hapus_absen" data-href="" data-bs-dismiss="modal">Hapus</a>
+                </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>
                     <button class="btn btn-primary">Event Page</button>
@@ -63,40 +66,31 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal"><span class="visually-hidden">close</span></button>
                 </div>
                 <div id="modalBody2" class="modal-body">
-                    <form>
+                    <form action="{{route('input_absen')}}" method="post">
+                        @csrf
                         <div class="mb-3">
                             <label for="formGroupExampleInput" class="form-label">Nama Siswa</label>
-                            <input type="text" class="form-control" id="nama" placeholder="Nama Siswa" autocomplete="off">
+                            <input type="text" class="form-control" id="nama" placeholder="Nama Siswa" name="nama_siswa" autocomplete="off">
                             <div id="result" class="result"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="formGroupExampleInput2" class="form-label">Another label</label>
-                            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
-                        </div>
-                    </form>
+                            <label for="formGroupExampleInput" class="form-label">Status Kehadiran</label>
+                            <select type="text" class="form-select" id="status_kehadiran" name="status_kehadiran">
+                                <option value="" selected disabled>Pilih Status</option>                               
+                                <option value="hadir">Hadir</option>  
+                                <option value="izin">Izin</option>  
+                                <option value="sakit">Sakit</option>  
+                            </select>
+                            
+                        </div>                                            
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>
-                    <button class="btn btn-primary">Add</button>
+                    <button class="btn btn-primary" type="submit">Add</button>
                 </div>
+            </form>
             </div>
         </div>
-
-        {{-- <div id="fullCalModal" class="modal fade">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 id="modalTitle1" class="modal-title"></h4>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal"><span class="visually-hidden">close</span></button>
-                    </div>
-                    <div id="modalBody1" class="modal-body"></div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>
-                        <button class="btn btn-primary">Event Page</button>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
 
     <script type="text/javascript">
         $('#get_jurusan').on('change', function getKelas(){
@@ -143,11 +137,15 @@
         <script>
             var result = document.getElementById("result");
             var id_jurusan;
+            var id_kelas;
             var count_siswa;
 	        result.innerHTML="";
 	        result.style.border="0px";
             $('#get_jurusan').on('change', function getKelas(){
                id_jurusan = $('#get_jurusan option:selected').val();
+            })
+            $('#get_kelas').on('change', function getKelas(){
+               id_kelas = $('#get_kelas option:selected').val();
             })
 	
             $('#nama').on('keyup', function() {
@@ -160,7 +158,7 @@
                 $.ajax({
                     url: `{{route('search_nama_siswa')}}`,
                     type: 'GET',
-                    data: {'search':$val, 'id_sekolah':{{session('id')}}, 'id_jurusan':id_jurusan},
+                    data: {'search':$val, 'id_sekolah':{{session('id')}}, 'id_jurusan':id_jurusan, 'id_kelas':id_kelas},
                     headers: {
 						'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 					},
@@ -177,9 +175,49 @@
 
             function namaSiswa(no){                            
                 var nama_siswa = $('.selection_'+no).val();
+                var id_siswa = $('.selection_'+no).data('id-siswa');
                 $('#nama').val(nama_siswa);
-                console.log(nama_siswa)
+                $('#id_siswa').val(id_siswa);
                 
             }
+
+            $('.hapus_absen').click(function (){
+                var getLink = $(this).data('href');
+                const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                    confirmButton: "btn btn-success",
+                    cancelButton: "btn btn-danger me-2",
+                },
+                buttonsStyling: false,
+            });
+
+            swalWithBootstrapButtons
+                .fire({
+                    title: "Are you sure?",
+                    text: "You won't be able to revert this!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonClass: "me-2",
+                    confirmButtonText: "Yes, delete it!",
+                    cancelButtonText: "No, cancel!",
+                    reverseButtons: true,
+                })
+                .then((result) => {
+                  console.log(result);
+                    if (result.isConfirmed) {
+                      window.location.href = getLink
+                        
+                    } else if (
+                        // Read more about handling dismissals
+                        result.dismiss === Swal.DismissReason.cancel
+                    ) {
+                        swalWithBootstrapButtons.fire(
+                            "Cancelled",
+                            "Your imaginary file is safe :)",
+                            "error"
+                        );
+                    }
+                });
+  });
         </script>            			
 @endsection
