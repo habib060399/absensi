d76225c911263c55ae9828f9e464afb7002366ce
habib060399 <habibsh180@gmail.com>
2024-08-13 17:02:15 +0700

    menambahkan key bc, token_wa dan token_account_wa

--- database/migrations/2024_03_11_102409_create_settings_table.php
+++ database/migrations/2024_03_11_102409_create_settings_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
+            $table->string('id_sekolah', 100);
+            $table->string('bc', 200);
+            $table->String('token_wa', 150);
+            $table->string('token_account_wa', 150);
             $table->timestamps();
         });
     }
