11372b9233982f939fa32ec1f8bc565d71592df6
habib060399 <habibsh180@gmail.com>
2026-05-03 07:57:08 +0700

    memperbaiki fitur rekapitulasi absensi

--- app/Exports/RekapAbsenPerSheet.php
+++ app/Exports/RekapAbsenPerSheet.php
@@ -9,10 +9,13 @@ use Maatwebsite\Excel\Concerns\FromArray;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 use App\Models\Absensi;
 use App\Models\Siswa;
 
-class RekapAbsenPerSheet implements FromArray, WithTitle, WithHeadings
+class RekapAbsenPerSheet implements FromArray, WithTitle, WithHeadings, WithStyles, WithEvents
 {
     // WithHeadings, WithStyles, FromArray,
     public $jurusan;
@@ -21,6 +24,7 @@ class RekapAbsenPerSheet implements FromArray, WithTitle, WithHeadings
     public $bulan;
     public $bulan_int;
     public $b = array();
+    public $tanggal;
 
     public function __construct($tahun, $bulan, $bulan_int, $jurusan, $kelas){
         $this->tahun = $tahun;
@@ -28,8 +32,76 @@ class RekapAbsenPerSheet implements FromArray, WithTitle, WithHeadings
         $this->bulan_int = $bulan_int;
         $this->jurusan = $jurusan;
         $this->kelas = $kelas;
+        $this->tanggal = cal_days_in_month(CAL_GREGORIAN, $bulan_int, $tahun); 
     }
 
+    public function styles(Worksheet $sheet){
+        return  [
+            1 => ['font' => ['bold' => true]],
+            2 => ['font' => ['bold' => true]],
+            3 => ['font' => ['bold' => true]],
+        ];
+
+    }
+
+    public function registerEvents(): array {        
+        return [
+            AfterSheet::class => function($event){
+                $lastColumn = Coordinate::stringFromColumnIndex($this->tanggal + 1);                
+                $nextColumn = Coordinate::stringFromColumnIndex(Coordinate::columnIndexFromString($lastColumn)+1);
+                $lastRow = count($this->b) + 2;
+                $columnHadir = Coordinate::stringFromColumnIndex($this->tanggal + 2);
+                // dd($columnHadir);
+
+                $event->sheet->mergeCells("A1:A3");
+                $event->sheet->mergeCells("B1:{$lastColumn}1");
+                $event->sheet->mergeCells("B2:{$lastColumn}2");
+                $event->sheet->setCellValue("A1", "NAMA SISWA");
+                $event->sheet->setCellValue("B1", $this->bulan);
+                $event->sheet->setCellValue("B2", "Tanggal");
+                $event->sheet->setCellValue($nextColumn."1", "Total");
+                $event->sheet->setCellValue(Coordinate::stringFromColumnIndex(Coordinate::columnIndexFromString($nextColumn))."3", "Hadir");
+                $event->sheet->setCellValue(Coordinate::stringFromColumnIndex(Coordinate::columnIndexFromString($nextColumn)+1)."3", "Izin");
+                $event->sheet->setCellValue(Coordinate::stringFromColumnIndex(Coordinate::columnIndexFromString($nextColumn)+2)."3", "Sakit");                
+
+                for ($row = 4; $row <= $lastRow; $row++){
+                    $event->sheet->setCellValue(
+                        "{$columnHadir}{$row}","=COUNTIF(B{$row}:{$lastColumn}{$row}, \"hadir\")"
+                    );
+                }
+
+                // border semua 
+                $event->sheet->getStyle("A1:{$lastColumn}{$lastRow}")->applyFromArray([
+                    'borders' => [
+                        'allBorders' => [
+                            'borderStyle' => 'thin',
+                        ]
+                    ]
+                ]);
+
+                // Header style
+                $event->sheet->getStyle("A1:{$lastColumn}3")->applyFromArray([
+                    'font' => [
+                        'bold' => true,
+                        'color' => ['argb' => 'FFFFFFF'],
+                    ],
+                    'alignment' => [
+                        'horizontal' => 'center',
+                        'vertical' => 'center',
+                    ],
+                    'fill' => [
+                        'fillType' => 'solid',
+                        'startColor' => ['argb' => '4F81BD'],
+                    ],
+                ]);
+
+                // auto size kolom
+                foreach (range('A', $lastColumn) as $col){
+                    $event->sheet->getColumnDimension($col)->setAutoSize(true);
+                }
+            }
+        ];
+    }
 
     public function array(): array {
         $siswa = Siswa::where('id_jurusan', $this->jurusan)->where('id_kelas', $this->kelas)->select('*')->get();
@@ -67,12 +139,18 @@ class RekapAbsenPerSheet implements FromArray, WithTitle, WithHeadings
     }
 
     public function headings(): array {
-        $tanggal = cal_days_in_month(CAL_GREGORIAN, $this->bulan_int, $this->tahun);
-        $heading =array('Nama Siswa');
-        for($i = 1; $i <= $tanggal; $i++){
-            $heading[($i+2)]=$i;
+        // $tanggal = cal_days_in_month(CAL_GREGORIAN, $this->bulan_int, $this->tahun); 
+        $row1 = [' '];
+        $row2 = [' '];
+        $row3 = [' '];
+        for($i = 1; $i <= $this->tanggal; $i++){                         
+            $row3[($i+2)]=$i;
         }
-        return $heading;
+        return [
+           $row1,
+           $row2,
+           $row3
+        ];
     }
 
     public function title(): string
